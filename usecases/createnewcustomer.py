02a21ca94aeba8907433cdcc1bb368e4eff8ac58
NIDHI RAI <nidhi.rai94@gmail.com>
2023-11-23 00:01:05 +0530

    Delete usecases directory

--- usecases/createnewcustomer.py
+++
@@ -1,6 +0,0 @@
-class CreateCustomer:
-    def __init__(self, customer_repository):
-        self.customer_repository = customer_repository
-
-    def create_customer(self, name, email, phone):
-        return self.customer_repository.create_customer(name, email, phone)
