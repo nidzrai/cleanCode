fa9d31ef97b927634ebd776190cdf8ff0e6436a9
NIDHI RAI <nidhi.rai94@gmail.com>
2023-11-23 00:03:15 +0530

    Add files via upload

--- src/test/domain/test_entities.py
+++ src/test/domain/test_entities.py
@@ -1,1 +1,26 @@
+import pytest
 
+from src.domain.entities.customer import Customer
+from src.domain.entities.exception import InvalidPhoneNumberException
+
+
+def test_customer_create():
+    customer = Customer(
+        name="test",
+        email="test@testdom.com",
+        phone="+1234567890"
+    )
+    assert customer
+    assert customer.name == "test"
+    assert customer.email == "test@testdom.com"
+    assert customer.phone == "+1234567890"
+
+
+def test_customer_create_throws_invalid_email_exception():
+    with pytest.raises(InvalidPhoneNumberException) as exc:
+        Customer(
+            name="test",
+            email="test@testdom.com",
+            phone="+12345678eqweqweqweqweqw90"
+        )
+    assert isinstance(exc.value, InvalidPhoneNumberException)
